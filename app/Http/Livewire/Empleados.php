e59dacbfc15dd4fde263441a242ea09390b4a506
Oliver Concepcion <oliver.cocco@gmail.com>
2022-10-03 19:31:34 -0400

    Initial commit

---
+++ app/Http/Livewire/Empleados.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Empleado;
+
+class Empleados extends Component
+{
+    use WithPagination;
+
+	protected $paginationTheme = 'bootstrap';
+    public $selected_id, $keyWord, ;
+    public $updateMode = false;
+
+    public function render()
+    {
+		$keyWord = '%'.$this->keyWord .'%';
+        return view('livewire.empleados.view', [
+            'empleados' => Empleado::latest()
+						->paginate(10),
+        ]);
+    }
+	
+    public function cancel()
+    {
+        $this->resetInput();
+        $this->updateMode = false;
+    }
+	
+    private function resetInput()
+    {		
+    }
+
+    public function store()
+    {
+        $this->validate([
+        ]);
+
+        Empleado::create([ 
+        ]);
+        
+        $this->resetInput();
+		$this->emit('closeModal');
+		session()->flash('message', 'Empleado Successfully created.');
+    }
+
+    public function edit($id)
+    {
+        $record = Empleado::findOrFail($id);
+
+        $this->selected_id = $id; 
+		
+        $this->updateMode = true;
+    }
+
+    public function update()
+    {
+        $this->validate([
+        ]);
+
+        if ($this->selected_id) {
+			$record = Empleado::find($this->selected_id);
+            $record->update([ 
+            ]);
+
+            $this->resetInput();
+            $this->updateMode = false;
+			session()->flash('message', 'Empleado Successfully updated.');
+        }
+    }
+
+    public function destroy($id)
+    {
+        if ($id) {
+            $record = Empleado::where('id', $id);
+            $record->delete();
+        }
+    }
+}
